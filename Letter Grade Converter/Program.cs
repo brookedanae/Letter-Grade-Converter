97b0b05f71e60433225108a20e1d1995b873a0c5
brooke <brookedanae88@gmail.com>
2021-01-31 19:21:29 -0500

    First commit

---
+++ Letter Grade Converter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Letter_Grade_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Letter Grade Converter!" + "\n");
+
+            string answer;
+            do
+            {
+                Console.Write("Enter a numerical grade: ");
+                var grade = int.Parse(Console.ReadLine());
+                Console.WriteLine(LetterGrade(grade));
+
+                Console.Write("Continue? (y/n) ");
+                answer = Console.ReadLine();
+            } while (answer == "y" || answer == "Y");
+
+            Console.WriteLine("Goodbye!");
+        }
+
+        
+        public static string LetterGrade(int grade)
+        {
+            string end = "";
+            if (grade >= 88 && grade <= 100)
+            {
+                Console.WriteLine("Letter Grade: A");
+            }
+            else if (grade >= 80 && grade <= 87)
+            {
+                end = "Letter Grade: B";
+            }
+            else if (grade >= 67 && grade <= 79)
+            {
+                end = "Letter Grade: C";
+            }
+            else if (grade >= 60 && grade <= 66)
+            {
+                end = "Letter Grade: D";
+            }
+            else if (grade < 60)
+            {
+                end = "Letter Grade: F";
+            }
+            return end;
+        }
+
+        
+    }
+
+}
+
